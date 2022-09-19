589d66da66330496bf01e77eec85defed98aa56c
ingvord <igor.khokhriakov@rcsb.org>
2022-09-19 11:41:02 -0700

    Read version from pom.xml

--- src/main/java/org/rcsb/Dummy.java
+++ src/main/java/org/rcsb/Dummy.java
@@ -5,7 +5,24 @@ import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Dummy {
+    private static final String VERSION;
+
+    static {
+        Properties pomProperties = new Properties();
+        try {
+            pomProperties.load(
+                    new BufferedInputStream(
+                        Dummy.class.getResourceAsStream("/pom.properties")
+            ));
+        } catch (IOException ignored) {}
+        VERSION = pomProperties.getProperty("project.version", "UNKNOWN");
+    }
+
     public static void main(String[] args) {
         Undertow server = Undertow.builder()
                 .addHttpListener(8080, "localhost")
@@ -13,7 +30,7 @@ public class Dummy {
                     @Override
                     public void handleRequest(final HttpServerExchange exchange) throws Exception {
                         exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                        exchange.getResponseSender().send("Hello World");
+                        exchange.getResponseSender().send(String.format("Hello World from version %s", VERSION));
                     }
                 }).build();
         server.start();
