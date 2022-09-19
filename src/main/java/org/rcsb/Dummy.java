5cf9f5527f59cb23dd8771b71016f8791e3d246e
ingvord <igor.khokhriakov@rcsb.org>
2022-09-19 11:01:18 -0700

    Dummy server

--- src/main/java/org/rcsb/Dummy.java
+++ src/main/java/org/rcsb/Dummy.java
@@ -1,7 +1,21 @@
 package org.rcsb;
 
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
 public class Dummy {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+                    @Override
+                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello World");
+                    }
+                }).build();
+        server.start();
     }
 }
